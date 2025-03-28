c787d51b24732eaf4cfddfa64f5426abace00c31
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-03-28 11:56:47 -0300

    feat(operadores-aritmeticos): adiciona divisão de valor entre 4 pessoas

---
+++ operadores_aritméticos/atv4.js
@@ -0,0 +1,8 @@
+let valor = 8;
+let pessoas = 2;
+let valorporpessoa = 0;
+    valorporpessoa = valor / pessoas;
+
+console.log('Cada pessoa recebera o valor de ' + valorporpessoa);
+
+//Rafael - 28/03 - 11:56
