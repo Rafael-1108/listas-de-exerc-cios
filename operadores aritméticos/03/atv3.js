7a47b8562b83e0925b7c006ad875c8d4d87693b1
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-03-28 11:36:48 -0300

    feat(operadores-aritmeticos): adiciona soma de dois números

---
+++ operadores aritméticos/03/atv3.js
@@ -0,0 +1,12 @@
+let n = 3;
+let quadrado = 0;
+    quadrado = n * n;
+
+let cubo = 0;
+    cubo = n * n * n;
+
+console.log(
+    "O quadrado docecdnúmero escolhido é " + quadrado + " e o cubo é " + cubo
+);
+
+//Rafael - 28/03 - 11:27
