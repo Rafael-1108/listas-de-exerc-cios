a2c1cb3ccc510b192ed9763b818e3f4d9b784814
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 16:44:27 -0300

    adiciona checagem de idade pra votar

---
+++ Operadores Lógicos/atv2.js
@@ -0,0 +1,11 @@
+let idade = 34
+
+    if (idade >= 16 && idade <= 70) {
+        console.log("Você pode votar!")
+}
+
+    else {
+        console.log("Você não pode votar!")
+}
+
+//Rafael - 03/04 - 16:43
