58dbc3c24dea1f5c3d270fbd6a85f64633a60964
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 20:30:22 -0300

    adiciona verificação de divisibilidade por 3 e 5

--- IfElse/atv4.js
+++ IfElse/atv4.js
@@ -1,10 +1,10 @@
-let numero = 13
+let numero = 13;
 
 if (numero %3 ==0 && numero %5 ==0) {
-    console.log("O número é divisível por 3 e por 5 ao mesmo tempo")
+    console.log("O número é divisível por 3 e por 5 ao mesmo tempo");
 }
 else {
-    console.log("O número não é divisível por 3 e por 5 ao mesmo tempo")
+    console.log("O número não é divisível por 3 e por 5 ao mesmo tempo");
 }
 
 //Rafael - 03/04 - 19:58
