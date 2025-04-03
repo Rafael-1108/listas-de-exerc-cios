10de1510b27ece4c6feafdec328864437f149962
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 20:37:37 -0300

    adiciona identificação do menor entre três números

---
+++ IfElse/atv8.js
@@ -0,0 +1,15 @@
+let numero1 = 8;
+let numero2 = 6;
+let numero3 = 2;
+
+if (numero1 < numero2 && numero1 < numero3) {
+    console.log("O primeiro número é o menor.");
+}
+    else if (numero2 < numero1 && numero2 < numero3) {
+        console.log("O segundo número é o menor.");
+    }
+else {
+    console.log("O terceiro número é o menor.")
+}
+
+//Rafael - 03/04 - 20:37
