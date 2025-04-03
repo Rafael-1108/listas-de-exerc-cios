3395cdbfef6ea487eadbee290d86f9b83669393c
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 19:55:24 -0300

    adiciona classificação etária por idade

---
+++ IfElse/atv3.js
@@ -0,0 +1,16 @@
+let idade = 0;
+
+if (idade <= 12) {
+    console.log("Você é uma criança");
+}
+    else if (idade <=17) {
+        console.log("Você é um adolescente");
+    }
+    else if (idade <= 59) {
+        console.log("Você é um adulto");
+    }
+else {
+    console.log("Você é um idoso");
+}
+
+//Rafael - 03/04 - 19:54
