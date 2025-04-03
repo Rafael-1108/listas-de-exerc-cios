452fac41b5532610bf81c837b377f25c51698096
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 20:43:54 -0300

    adiciona classificação de clima por temperatura

---
+++ IfElse/atv9.js
@@ -0,0 +1,13 @@
+let temperatura = 40;
+
+if (temperatura <15) {
+    console.log("Está frio");
+}
+    else if (temperatura <= 25) {
+        console.log("Está agradável");
+    }
+else {
+    console.log("Está quente");
+}
+
+//Rafael - 03/04 - 20:42
