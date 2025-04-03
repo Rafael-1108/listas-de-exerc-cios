13fb643dc80a5ebfe5f218746bab32b13f44f77b
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 20:31:02 -0300

    adiciona comparação do maior entre dois números

--- IfElse/atv2.js
+++ IfElse/atv2.js
@@ -1,14 +1,14 @@
-let numero1 = 5
-let numero2 = 8
+let numero1 = 5;
+let numero2 = 8;
 
 if (numero1 - numero2 >0) {
-    console.log("O primeiro número é o maior")
+    console.log("O primeiro número é o maior");
 }
 else if (numero1 - numero2 ==0) {
-    console.log("Os números são iguais")
+    console.log("Os números são iguais");
 }
 else { 
-    console.log("O segundo número é o maior")
+    console.log("O segundo número é o maior");
 }
 
 //Rafael - 03/04 - 19:35
