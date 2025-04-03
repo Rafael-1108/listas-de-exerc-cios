38c54c53775693eea2473662c11fde39d9cbd426
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 19:17:25 -0300

    adiciona operação AND com booleanos

---
+++ IfElse/atv1.js
@@ -0,0 +1,11 @@
+let numero = 3;
+
+if (numero >0) {
+    console.log("O número é positivo");
+}
+else if (numero = 0) {
+    console.log("Número neutro")
+}
+else if
+
+//Rafael - 03/04 - 18:54
--- Operadores Lógicos/atv4.js
+++ Operadores Lógicos/atv4.js
@@ -1,3 +1,11 @@
-let valor = 4;
-let valor2 = 5;
+let valor = true;
+let valor2 = false;
 
+if (valor && valor2) {
+    console.log("É verdade");
+}
+else {
+    console.log("É mentira");
+}
+
+//Rafael - 03/04 - 16:40
