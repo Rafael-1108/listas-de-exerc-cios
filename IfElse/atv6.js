12900421b1a9d7f2871cb5a3e4d8b6d78983ea22
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 20:29:44 -0300

    adiciona verificação de par ou ímpar

--- IfElse/atv6.js
+++ IfElse/atv6.js
@@ -1,10 +1,10 @@
-let numero = 2
+let numero = 2;
 
 if (numero %2 ==0) {
-    console.log("Número par.")
+    console.log("Número par.");
 }
 else {
-    console.log("Número ímpar")
+    console.log("Número ímpar");
 }
 
 //Rafael - 03/04 - 20:18
