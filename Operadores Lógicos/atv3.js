dfa53e875279c11e30534a564966fc41efa7e4e2
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 16:50:48 -0300

    adiciona verificação de número maior que 100

---
+++ Operadores Lógicos/atv3.js
@@ -0,0 +1,13 @@
+let n1 = 4
+let n2 = 50
+let n3 = 100.1
+
+    if (n1 >100 || n2 >100 || n3 >100) {
+        console.log("Pelo menos um dos 3 números é maior que 100!")
+}
+
+    else {
+        console.log("Nenhum dos 3 números é maior do que 100!")
+    }
+
+//Rafael - 03/04 - 16:50
