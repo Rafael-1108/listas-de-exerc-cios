6d8a92d69d7bfff1e1221d093b28ef61644be6d6
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 19:27:54 -0300

    adiciona verificação de positivo, negativo ou zero

--- IfElse/atv1.js
+++ IfElse/atv1.js
@@ -1,11 +1,16 @@
-let numero = 3;
+let numero = 0;
 
 if (numero >0) {
     console.log("O número é positivo");
 }
-else if (numero = 0) {
-    console.log("Número neutro")
+else if (numero ==0) {
+    console.log("Número neutro");
 }
-else if
+else if (cumero <0) {
+    console.log("O número é negativo");
+}
+else {
+    console.log("Número inválido");
+}
 
 //Rafael - 03/04 - 18:54
