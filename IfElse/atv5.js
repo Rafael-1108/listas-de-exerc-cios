1721fe56cca0e816b92ec3cb371abde0881c6d84
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 20:04:22 -0300

    adiciona verificação de aprovação por nota

---
+++ IfElse/atv5.js
@@ -0,0 +1,13 @@
+let nota = 70
+
+if (nota >= 60 && nota <= 100) {
+    console.log("Aprovado!")
+}
+    else if (nota < 60) {
+        console.log("Reprovado.")
+}
+else {
+    console.log("Nota inválida")
+}
+
+//Rafael - 03/04 - 20:04
