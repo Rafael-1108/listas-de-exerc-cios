5b1bb416ac56df5d017632ec24c82170c86f457b
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 21:12:04 -0300

    adiciona verificação de ano bissexto

---
+++ IfElse/atv10.js
@@ -0,0 +1,13 @@
+let ano = 364;
+
+if (ano % 366 == 0) {
+    console.log("Ano bissexto.");
+}
+    else if (ano % 365 == 0) {
+        console.log("Ano normal");
+    }
+else {
+    console.log("Quantidade de dias no ano inválida.");
+}
+
+//Rafael - 03/04 - 22:11
