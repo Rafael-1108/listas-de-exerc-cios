fa82f65f7a87915e7f5bdc129f85465e82474550
Rafael <rafael.s.mendes.1108@gmail.com>
2025-04-04 10:33:30 -0300

    adiciona exibição da estação do ano

---
+++ Switch_case/atv5.js
@@ -0,0 +1,18 @@
+let estacao = 2
+
+switch (estacao) {
+    case 1:
+        console.log("Verão")
+break;
+    case 2:
+        console.log("Outono")
+break;
+    case 3:
+        console.log("Outono")
+break;
+    case 4:
+        console.log("Primavera")
+break;
+    default:
+        console.log("Estação inváliva, selecione outra entre 1 e 4.")
+}
