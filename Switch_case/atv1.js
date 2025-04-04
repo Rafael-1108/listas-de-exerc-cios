878dce40aa52896ced52b6e4ffbb68368991b206
Rafael Santos <rafael.s.mendes.1108@gmail.com>
2025-04-03 21:30:39 -0300

    adiciona exibição do dia da semana

---
+++ Switch_case/atv1.js
@@ -0,0 +1,26 @@
+let dia = 3;
+
+if (dia ==1) {
+    console.log("É domingo.");
+}
+    else if (dia ==2) {
+        console.log("É segunda.");
+}
+    else if (dia ==3) {
+        console.log("É Terça.");
+}
+    else if (dia ==4) {
+        console.log("É quarta.");
+}
+    else if (dia ==5) {
+        console.log("É quinta.");
+}
+    else if (dia ==6) {
+        console.log("É sexta.");
+}
+    else if (dia ==7) {
+        console.log("É sábado.");
+}
+else {
+    console.log("Dia inválido.");
+}
